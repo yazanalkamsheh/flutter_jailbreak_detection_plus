de80db65a055f11a3408d7533576439fa9bb74b6
Aziz Soulé <aremou.soule30@ufhb.edu.ci>
2025-04-06 13:56:45 +0000

    Fix import

--- ios/Classes/FlutterJailbreakDetectionPlugin.m
+++ ios/Classes/FlutterJailbreakDetectionPlugin.m
@@ -5,7 +5,7 @@
     // Support project import fallback if the generated compatibility header
     // is not copied when this plugin is created as a library.
     // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-    #import "flutter_jailbreak_detection-Swift.h"
+    #import "flutter_jailbreak_detection_plus-Swift.h"
 #endif
 
 @implementation FlutterJailbreakDetectionPlugin
